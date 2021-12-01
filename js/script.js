41da6353a384d403a0d389becd4b6b55b3f3d2f9
monthebrice2000 <monthedjeumoubrice2000@gmail.com>
2021-12-01 12:44:13 +0100

    Add file for animation and and add heading

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+$("document").ready( function() {
+    $("a").click(function () {
+        $("nav > ul > li > a").removeClass("active");
+        $(this).toggleClass("active");
+    });
+
+    // $("main > div > ul > li").click(function () {
+    //     if( ! $(this).is("main > div > ul > li:last-child") ){
+    //         $("main > div > ul > li").removeClass("active");
+    //         $(this).toggleClass("active");
+    //         $("main .main_description").text( $(this).text() );
+    //     }
+    // });
+
+
+})
+
