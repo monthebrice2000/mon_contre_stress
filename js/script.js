3633337edad2fa3b6d0607778401640010fb6b21
monthebrice2000 <monthedjeumoubrice2000@gmail.com>
2021-12-02 20:17:15 +0100

    Add file for C/C++ Animations

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ $("document").ready( function() {
     $("a").click(function () {
         $("nav > ul > li > a").removeClass("active");
         $(this).toggleClass("active");
+
     });
+
+
 
     // $("main > div > ul > li").click(function () {
     //     if( ! $(this).is("main > div > ul > li:last-child") ){
@@ -13,6 +16,7 @@ $("document").ready( function() {
     //         $("main .main_description").text( $(this).text() );
     //     }
     // });
+
 
 
 })
